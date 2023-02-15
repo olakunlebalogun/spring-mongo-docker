63a03a383284977e20edd2471512aecd5b89f27b
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-02-15 12:57:12 +0100

    created the base classes needed for this configuration

---
+++ src/main/java/com/olakunle/entities/Address.java
@@ -0,0 +1,7 @@
+package com.olakunle.entities;
+
+public class Address {
+    private String country;
+    private String postCode;
+    private String city;
+}
---
+++ src/main/java/com/olakunle/entities/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.olakunle.entities.enums;
+
+public enum Gender {
+    MALE, FEMALE;
+}
